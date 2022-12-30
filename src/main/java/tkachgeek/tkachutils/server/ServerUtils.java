a7828fc712a563d7512820515e87007573fd74df
Soul_KRT <soul.krt.00@gmail.com>
2022-12-31 02:52:20 +0300

    update ServerUtils: optimize code in method isVersionBefore1_16_5

--- src/main/java/tkachgeek/tkachutils/server/ServerUtils.java
+++ src/main/java/tkachgeek/tkachutils/server/ServerUtils.java
@@ -5,19 +5,12 @@ import tkachgeek.tkachutils.numbers.NumbersUtils;
 
 public class ServerUtils {
     public static boolean isVersionBefore1_16_5() {
-        String[] version = Bukkit.getBukkitVersion().substring(0, 6).split("\\.", 3);
+        String version = Bukkit.getBukkitVersion()
+                .substring(0, 6)
+                .replaceAll("\\.", "");
 
-        StringBuilder raw = new StringBuilder();
-        if (version.length == 3) {
-            for (String number : version) {
-                if (NumbersUtils.isInteger(number)) {
-                    raw.append(number);
-                } else {
-                    return true;
-                }
-            }
-
-            int number = Integer.parseInt(raw.toString());
+        if (NumbersUtils.isInteger(version)) {
+            int number = Integer.parseInt(version);
             return number < 1165;
         }
 
