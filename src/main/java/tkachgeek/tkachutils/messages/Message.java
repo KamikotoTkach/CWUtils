43d0b44eee83a6c6116a69f35072637e35f398fb
Soul_KRT <soul.krt.00@gmail.com>
2023-01-09 17:50:36 +0300

    rework Message

--- src/main/java/tkachgeek/tkachutils/messages/Message.java
+++ src/main/java/tkachgeek/tkachutils/messages/Message.java
@@ -6,30 +6,38 @@ import org.bukkit.command.CommandSender;
 import tkachgeek.tkachutils.server.ServerUtils;
 
 public class Message {
-    private final String message;
+   private final String message;
 
-    public Message(String message) {
-        this.message = message;
-    }
+   public Message(String message) {
+      this.message = message;
+   }
 
-    public Message(Component message) {
-        this(LegacyComponentSerializer.legacySection().serialize(message));
-    }
+   public Message(Component message) {
+      this(LegacyComponentSerializer.legacySection().serialize(message));
+   }
 
-    public Builder builder() {
-        return new Builder(this.message);
-    }
+   private Message placeholder(String placeholder, String value) {
+      return new Placeholder(this.message).replacePlaceholders(placeholder, value);
+   }
 
-    public Component get() {
-        return LegacyComponentSerializer.legacySection().deserialize(message);
-    }
+   public Message placeholder(String placeholder, Object value) {
+      return this.placeholder(placeholder, String.valueOf(value));
+   }
 
-    public void send(CommandSender sender) {
-        if (ServerUtils.isVersionBefore1_16_5()) {
-            sender.sendMessage(message);
-            return;
-        }
+   public Message placeholder(String placeholder, Component value) {
+      return this.placeholder(placeholder, LegacyComponentSerializer.legacySection().serialize(value).replaceAll("\\\\", ""));
+   }
 
-        sender.sendMessage(this::get);
-    }
+   public Component get() {
+      return LegacyComponentSerializer.legacySection().deserialize(this.message);
+   }
+
+   public void send(CommandSender sender) {
+      if (ServerUtils.isVersionBefore1_16_5()) {
+         sender.sendMessage(this.message);
+         return;
+      }
+
+      sender.sendMessage(this::get);
+   }
 }
---
+++ src/main/java/tkachgeek/tkachutils/messages/Placeholder.java
@@ -0,0 +1,39 @@
+package tkachgeek.tkachutils.messages;
+
+class Placeholder {
+   private enum PlaceholderSymbols {
+      left("<"),
+      right(">");
+
+      private final String symbol;
+
+      PlaceholderSymbols(String symbol) {
+         this.symbol = symbol;
+      }
+
+      public String getSymbol() {
+         return this.symbol;
+      }
+   }
+
+   private String message;
+
+   Placeholder(String message) {
+      this.message = message;
+   }
+
+   private String formatPlaceholder(String placeholder) {
+      if (placeholder.startsWith(PlaceholderSymbols.left.getSymbol())
+            && placeholder.endsWith(PlaceholderSymbols.right.getSymbol())) {
+         return placeholder;
+      }
+
+      return PlaceholderSymbols.left.getSymbol() + placeholder + PlaceholderSymbols.right.getSymbol();
+   }
+
+   Message replacePlaceholders(String placeholder, String value) {
+      placeholder = formatPlaceholder(placeholder);
+      this.message = this.message.replaceAll(placeholder, value);
+      return new Message(this.message);
+   }
+}
