d840c442d53f7cd0b4f2f3aa0252d2609cf19505
Soul_KRT <soul.krt.00@gmail.com>
2023-01-11 22:07:48 +0300

    update ConfigUtils: add loadItemStack method

--- src/main/java/tkachgeek/tkachutils/config/ConfigUtils.java
+++ src/main/java/tkachgeek/tkachutils/config/ConfigUtils.java
@@ -6,7 +6,18 @@ import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import tkachgeek.tkachutils.messages.Message;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ConfigUtils {
@@ -52,5 +63,64 @@ public class ConfigUtils {
          }
       }
       return components;
+   }
+
+   public static ItemStack loadItemStack(YamlConfiguration config, String path) {
+      String item_type_name = config.getString(path + ".type", "dirt").toUpperCase();
+      Material item_type = Material.getMaterial(item_type_name);
+      if (item_type == null || item_type.isAir()) {
+         item_type = Material.DIRT;
+         new Message("§c[§4" + path + "§c]: предмет §4" + item_type_name + "§c не существует!")
+               .send(Bukkit.getConsoleSender());
+      }
+
+      int item_amount = Math.max(Math.min(config.getInt(path + ".amount", 1), 64), 1);
+
+      Component item_name = ConfigUtils.getComponent(path + ".name");
+      Component[] item_lore = ConfigUtils.getComponents(config.getStringList(path + ".lore"));
+
+      ItemStack item = new ItemStack(item_type, item_amount);
+
+      ItemMeta item_meta;
+      item_meta = item.getItemMeta();
+      item_meta.displayName(item_name);
+      item_meta.lore(Arrays.asList(item_lore));
+
+      if (item_meta instanceof PotionMeta) {
+         if (config.contains(path + ".effects")) {
+            for (String potion_effect : config.getConfigurationSection(path + ".effects").getKeys(false)) {
+               PotionEffectType effect_type = PotionEffectType.getByName(potion_effect.toUpperCase());
+               if (effect_type != null) {
+                  int duration = Math.max(config.getInt(path + ".effects." + potion_effect + ".duration", 1), 1) * 20;
+                  int level = Math.max(config.getInt(path + ".effects." + potion_effect + ".level", 1), 1) - 1;
+                  ((PotionMeta) item_meta).addCustomEffect(new PotionEffect(effect_type, duration, level), false);
+               } else {
+                  new Message("§c[§4" + path + "§c]: эффект §4" + potion_effect + "§c не существует!")
+                        .send(Bukkit.getConsoleSender());
+               }
+            }
+         }
+      }
+
+      if (config.contains(path + ".enchantments")) {
+         for (String enchantment_name : config.getConfigurationSection(path + ".enchantments").getKeys(false)) {
+            Enchantment enchantment = Enchantment.getByName(enchantment_name);
+            if (enchantment != null) {
+               int level = Math.max(config.getInt(path + ".enchantments." + enchantment_name, 1), 1);
+               if (!item_meta.addEnchant(enchantment, level, true)) {
+                  new Message("§c[§4" + path + "§c]: уровень зачарования §4" + enchantment_name + "§c задан некорректно!")
+                        .send(Bukkit.getConsoleSender());
+                  new Message("§cЕсли проблема не разрешаема, обратитесь к разработчику -> §dcwcode.ru/vk")
+                        .send(Bukkit.getConsoleSender());
+               }
+            } else {
+               new Message("§c[§4" + path + "§c]: зачарование §4" + enchantment_name + "§c не существует!");
+            }
+         }
+      }
+
+      item.setItemMeta(item_meta);
+
+      return item;
    }
 }
