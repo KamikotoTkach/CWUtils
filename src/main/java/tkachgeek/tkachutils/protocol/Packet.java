35b239325d97f2882e6a0676fe21cebaa5b1f094
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-04-05 21:26:26 +0300

    +packets setEntityStatus && setEntityFrozen

--- src/main/java/tkachgeek/tkachutils/protocol/Packet.java
+++ src/main/java/tkachgeek/tkachutils/protocol/Packet.java
@@ -3,7 +3,9 @@ package tkachgeek.tkachutils.protocol;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -53,6 +55,36 @@ public class Packet {
       ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
     } catch (InvocationTargetException ignored) {
     
+    }
+  }
+  
+  public static void setEntityStatus(Player receiver, Entity entity, byte status) {
+    PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_METADATA);
+    packet.getIntegers().write(0, entity.getEntityId()); //Set packet's entity id
+    WrappedDataWatcher watcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
+    WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
+    watcher.setEntity(entity); //Set the new data watcher's target
+    watcher.setObject(0, serializer, (byte) (0x40)); //Set status to glowing, found on protocol page
+    packet.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects()); //Make the packet's datawatcher the one we created
+    try {
+      ProtocolLibrary.getProtocolManager().sendServerPacket(receiver, packet);
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public static void setEntityFrozen(Player receiver, Entity entity, int ticksFrozen) {
+    PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_METADATA);
+    packet.getIntegers().write(0, entity.getEntityId()); //Set packet's entity id
+    WrappedDataWatcher watcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
+    WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Integer.class); //Found this through google, needed for some stupid reason
+    watcher.setEntity(entity); //Set the new data watcher's target
+    watcher.setObject(7, serializer, ticksFrozen); //Set status to glowing, found on protocol page
+    packet.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects()); //Make the packet's datawatcher the one we created
+    try {
+      ProtocolLibrary.getProtocolManager().sendServerPacket(receiver, packet);
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
     }
   }
 }
