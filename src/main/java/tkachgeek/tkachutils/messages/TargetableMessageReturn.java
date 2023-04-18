e91b19e4e2fbd3d70677c83c230673292c0f88b0
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-04-18 21:54:59 +0300

    +TargetableMessageReturn

---
+++ src/main/java/tkachgeek/tkachutils/messages/TargetableMessageReturn.java
@@ -0,0 +1,18 @@
+package tkachgeek.tkachutils.messages;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.CommandSender;
+
+import java.util.function.Function;
+
+public class TargetableMessageReturn extends Exception {
+  Function<CommandSender, Component> messageGetter;
+  
+  public TargetableMessageReturn(Function<CommandSender, Component> messageGetter) {
+    this.messageGetter = messageGetter;
+  }
+  
+  public Component getMessage(CommandSender sender) {
+    return messageGetter.apply(sender);
+  }
+}
