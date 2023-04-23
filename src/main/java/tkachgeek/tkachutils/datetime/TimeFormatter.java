074a7ff7e0795d0fae66fd809dbc21a647217213
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-04-23 15:39:50 +0300

    +TimeFormatter методы isAfter, isBefore, unix, formatNow и +форматы HH:mm:ss, yyyy.MM.dd HH:mm:ss

--- src/main/java/tkachgeek/tkachutils/datetime/TimeFormatter.java
+++ src/main/java/tkachgeek/tkachutils/datetime/TimeFormatter.java
@@ -4,14 +4,56 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeFormatter {
-  private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("mm:ss");
+  private static final SimpleDateFormat MM_SS_TIME_FORMAT = new SimpleDateFormat("mm:ss");
+  private static final SimpleDateFormat HH_MM_SS_TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
+  private static final SimpleDateFormat FULL_DATE_FORMAT = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
   
   public static String format(long millis, String format) {
     SimpleDateFormat dateFormat = new SimpleDateFormat(format);
     return dateFormat.format(new Date(millis));
   }
   
+  public static String format(long millis, SimpleDateFormat format) {
+    return format.format(new Date(millis));
+  }
+  
   public static String format(long millis) {
-    return SIMPLE_DATE_FORMAT.format(new Date(millis));
+    return format(millis, MM_SS_TIME_FORMAT);
+  }
+  
+  public static String formatFull(long millis) {
+    return format(millis, FULL_DATE_FORMAT);
+  }
+  
+  public static String formatHours(long millis) {
+    return format(millis, HH_MM_SS_TIME_FORMAT);
+  }
+  
+  public static String formatNowFull() {
+    return format(System.currentTimeMillis(), FULL_DATE_FORMAT);
+  }
+  
+  public static String formatNowHours() {
+    return format(System.currentTimeMillis(), HH_MM_SS_TIME_FORMAT);
+  }
+  
+  public static String formatNow(String format) {
+    return format(System.currentTimeMillis(), format);
+  }
+  
+  public static String formatNow() {
+    return format(System.currentTimeMillis());
+  }
+  
+  public static long unix() {
+    return System.currentTimeMillis() / 1000;
+  }
+  
+  public static boolean isBefore(long millis) {
+    return System.currentTimeMillis() < millis;
+  }
+  
+  public static boolean isAfter(long millis) {
+    return System.currentTimeMillis() > millis;
   }
 }
