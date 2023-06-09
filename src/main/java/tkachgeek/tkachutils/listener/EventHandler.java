c819d145617c2cdae47baef3d303f7642800267b
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-06-09 15:13:55 +0300

    + функциональный EventHandler с возможностью авто-удаления

---
+++ src/main/java/tkachgeek/tkachutils/listener/EventHandler.java
@@ -0,0 +1,72 @@
+package tkachgeek.tkachutils.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class EventHandler<E extends Event> implements Listener {
+  
+  private boolean isUnregister = false;
+  
+  public EventHandler(Class<? extends Event> eventClass, TemporaryEvent<E> event, @NotNull Plugin plugin) {
+    this(eventClass, EventPriority.NORMAL, plugin, event);
+  }
+  
+  public EventHandler(Class<? extends Event> eventClass, EventPriority priority, @NotNull Plugin plugin, TemporaryEvent<E> event) {
+    Bukkit.getServer().getPluginManager().registerEvent(eventClass, this, priority,
+                                                        (ignored, ev) -> {
+                                                          try {
+                                                            event.use((E) ev);
+                                                          } catch (ClassCastException e) {
+                                                          }
+                                                        },
+                                                        
+                                                        plugin);
+  }
+  
+  public EventHandler(Class<? extends Event> eventClass, @NotNull Plugin plugin, TemporaryEventAutoUnregister<E> event) {
+    this(eventClass, EventPriority.NORMAL, plugin, event);
+  }
+  
+  public EventHandler(Class<? extends Event> eventClass, EventPriority priority, @NotNull Plugin plugin, TemporaryEventAutoUnregister<E> event) {
+    Bukkit.getServer().getPluginManager().registerEvent(eventClass, this, priority,
+                                                        (ignored, ev) -> {
+                                                          try {
+                                                            if (isUnregister) return;
+                                                            
+                                                            unregister(event.use((E) ev));
+                                                          } catch (ClassCastException e) {
+                                                          }
+                                                        }, plugin);
+  }
+  
+  public void unregister(boolean bool) {
+    if (bool) unregister();
+  }
+  
+  public void unregister() {
+    HandlerList.unregisterAll(this);
+    isUnregister = true;
+  }
+  
+  public boolean isUnregister() {
+    return isUnregister;
+  }
+  
+  public interface TemporaryEvent<E extends Event> {
+    
+    void use(E event);
+  }
+  
+  public interface TemporaryEventAutoUnregister<E extends Event> {
+    
+    /**
+     * Return true if you want to remove the listener
+     */
+    boolean use(E event);
+  }
+}
