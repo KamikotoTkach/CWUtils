66b5e18a851600a28c7815022968839fa4466652
Soul_KRT <soul.krt.00@gmail.com>
2023-06-19 16:50:49 +0300

    add WorldUtils add getFirstAvailableID in CollectionUtils

---
+++ src/main/java/tkachgeek/tkachutils/word/WorldUtils.java
@@ -0,0 +1,34 @@
+package tkachgeek.tkachutils.word;
+
+import org.bukkit.*;
+
+public class WorldUtils {
+   public static World createWorld(String name, WorldType worldType) {
+      if (Bukkit.getWorld(name) != null) {
+         return null;
+      }
+
+      World world = WorldCreator.name(name)
+                                .type(worldType)
+                                .createWorld();
+
+      return world;
+   }
+
+   public static World createFlatWorld(String name) {
+      World world = createWorld(name, WorldType.FLAT);
+      if (world != null) {
+         world.getPopulators().clear();
+
+         world.setGameRule(GameRule.RANDOM_TICK_SPEED, 0);
+         world.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+         world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+         world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+
+         world.setTime(6000);
+         world.setClearWeatherDuration(1000);
+      }
+
+      return world;
+   }
+}
