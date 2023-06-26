4b8bbd8205129aafec56f0e10f95d9870e75ca4f
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-06-26 21:58:42 +0300

    Declinations refactor

--- src/main/java/tkachgeek/tkachutils/text/Declinations.java
+++ src/main/java/tkachgeek/tkachutils/text/Declinations.java
@@ -5,35 +5,52 @@ import org.bukkit.Bukkit;
 import java.util.HashMap;
 
 public class Declinations {
-    static HashMap<String, HashMap<DeclinationsType, String>> wordlist = new HashMap<>();
-
-    public static void registerForNumbers(String key, String _1, String _234, String _056789) {
-        wordlist.put(key, new HashMap<>());
-        wordlist.get(key).put(DeclinationsType.nominative, _1);
-        wordlist.get(key).put(DeclinationsType.genitive, _234);
-        wordlist.get(key).put(DeclinationsType.multiple_genitive, _056789);
+  static HashMap<String, HashMap<DeclinationsType, String>> wordlist = new HashMap<>();
+  
+  public static void registerForNumbers(String key, String _1, String _234, String _056789) {
+    HashMap<DeclinationsType, String> map = getOrDefault(key);
+    
+    map.put(DeclinationsType.nominative, _1);
+    map.put(DeclinationsType.genitive, _234);
+    map.put(DeclinationsType.multiple_genitive, _056789);
+  }
+  
+  private static HashMap<DeclinationsType, String> getOrDefault(String key) {
+    if (!wordlist.containsKey(key)) {
+      return wordlist.put(key, new HashMap<>());
+    } else {
+      return wordlist.get(key);
     }
-
-    public static String cast(String key, int number) {
-        number = Math.abs(number);
-        if (number > 100) {
-            number %= 100;
-        }
-        if (number > 20) {
-            number %= 20;
-        }
-
-        if (!wordlist.containsKey(key)) Bukkit.getLogger().warning("Склонения для " + key + " не зарегистрированы");
-
-        switch (number) {
-            case 1:
-                return wordlist.getOrDefault(key, new HashMap<>()).getOrDefault(DeclinationsType.nominative, key);
-            case 2:
-            case 3:
-            case 4:
-                return wordlist.getOrDefault(key, new HashMap<>()).getOrDefault(DeclinationsType.genitive, key);
-            default:
-                return wordlist.getOrDefault(key, new HashMap<>()).getOrDefault(DeclinationsType.multiple_genitive, key);
-        }
+  }
+  
+  public static void register(String key, DeclinationsType type, String value) {
+    getOrDefault(key).put(type, value);
+  }
+  
+  public static String cast(String key, int number) {
+    number = Math.abs(number);
+    if (number > 100) {
+      number %= 100;
     }
+    if (number > 20) {
+      number %= 20;
+    }
+    
+    if (!wordlist.containsKey(key)) Bukkit.getLogger().warning("Склонения для " + key + " не зарегистрированы");
+    
+    switch (number) {
+      case 1:
+        return cast(key, DeclinationsType.nominative);
+      case 2:
+      case 3:
+      case 4:
+        return cast(key, DeclinationsType.genitive);
+      default:
+        return cast(key, DeclinationsType.multiple_genitive);
+    }
+  }
+  
+  public static String cast(String key, DeclinationsType type) {
+    return getOrDefault(key).getOrDefault(type, key);
+  }
 }
