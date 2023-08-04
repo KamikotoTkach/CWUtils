c647729bceca91fea54683a7f4eac0339a0bc214
Soul_KRT <soul.krt.00@gmail.com>
2023-08-04 19:09:40 +0300

    fix Hologram, add ParticlesUtils

--- src/main/java/tkachgeek/tkachutils/entity/Hologram.java
+++ src/main/java/tkachgeek/tkachutils/entity/Hologram.java
@@ -8,17 +8,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.UUID;
 
 public class Hologram {
-  
-  public static UUID showText(String text, int ticksToRemove, Location loc, JavaPlugin plugin) {
-    return loc.getWorld().spawn(loc, ArmorStand.class, armorStand -> {
-      armorStand.setVisible(false);
-      armorStand.setCustomName(text);
-      armorStand.setCustomNameVisible(true);
-      armorStand.setMarker(true);
-      armorStand.setCanMove(false);
-      armorStand.addScoreboardTag("hologram");
-      armorStand.setCanTick(false);
-      if (ticksToRemove > 0) Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, armorStand::remove, ticksToRemove);
-    }).getUniqueId();
-  }
+   public static void showTextAsync(String text, int ticksToRemove, Location loc, JavaPlugin plugin) {
+      Bukkit.getScheduler().runTask(plugin, () -> spawnHologram(text, ticksToRemove, loc, plugin));
+   }
+
+   public static UUID showText(String text, int ticksToRemove, Location loc, JavaPlugin plugin) {
+      if (Bukkit.isPrimaryThread()) {
+         return spawnHologram(text, ticksToRemove, loc, plugin);
+      } else {
+         showTextAsync(text, ticksToRemove, loc, plugin);
+         plugin.getLogger().warning("Asynchronous entity add!");
+         return null;
+      }
+   }
+
+   private static UUID spawnHologram(String text, int ticksToRemove, Location loc, JavaPlugin plugin) {
+      return loc.getWorld().spawn(loc, ArmorStand.class, armorStand -> {
+         armorStand.setVisible(false);
+         armorStand.setCustomName(text);
+         armorStand.setCustomNameVisible(true);
+         armorStand.setMarker(true);
+         armorStand.setCanMove(false);
+         armorStand.addScoreboardTag("hologram");
+         armorStand.setCanTick(false);
+         if (ticksToRemove > 0)
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, armorStand::remove, ticksToRemove);
+      }).getUniqueId();
+   }
 }
