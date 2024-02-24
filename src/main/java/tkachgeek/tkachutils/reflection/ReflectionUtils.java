514a0598c9431c430437d588f94d381d14f925b4
DESKTOP\PC <tkach@cwcode.ru>
2024-02-24 20:29:06 +0300

    +фильтрация при получении классов

--- src/main/java/tkachgeek/tkachutils/reflection/ReflectionUtils.java
+++ src/main/java/tkachgeek/tkachutils/reflection/ReflectionUtils.java
@@ -5,6 +5,7 @@ import java.lang.reflect.*;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -38,13 +39,17 @@ public class ReflectionUtils {
   }
 
   public static Set<Class<?>> getClasses(File jarFile, String packageName) {
+    return getClasses(jarFile, packageName, name -> true);
+  }
+  
+  public static Set<Class<?>> getClasses(File jarFile, String packageName, Predicate<String> filter) {
     Set<Class<?>> classes = new HashSet<>();
     try {
       JarFile file = new JarFile(jarFile);
       for (Enumeration<JarEntry> entry = file.entries(); entry.hasMoreElements(); ) {
         JarEntry jarEntry = entry.nextElement();
         String name = jarEntry.getName().replace("/", ".");
-        if (name.startsWith(packageName) && name.endsWith(".class"))
+        if (name.startsWith(packageName) && name.endsWith(".class") && filter.test(name))
           classes.add(Class.forName(name.substring(0, name.length() - 6)));
       }
       file.close();
