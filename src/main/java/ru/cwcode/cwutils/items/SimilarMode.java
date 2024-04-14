d93f265a10f3051bdd77d216c37c661e52e2f076
Soul_KRT <soul.krt.00@gmail.com>
2024-04-14 14:58:10 +0300

    try fix SimilarMode (equals between Component)

--- src/main/java/ru/cwcode/cwutils/items/SimilarMode.java
+++ src/main/java/ru/cwcode/cwutils/items/SimilarMode.java
@@ -1,6 +1,7 @@
 package ru.cwcode.cwutils.items;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 
@@ -42,7 +43,10 @@ public enum SimilarMode {
         if (item1DisplayName == null && item2DisplayName == null) return true;
         if (item1DisplayName == null || item2DisplayName == null) return false;
         
-        return item1DisplayName.equals(item2DisplayName);
+        String item1DisplayNameText = LegacyComponentSerializer.legacySection().serialize(item1DisplayName);
+        String item2DisplayNameText = LegacyComponentSerializer.legacySection().serialize(item2DisplayName);
+        
+        return item1DisplayNameText.equals(item2DisplayNameText);
       }
       
       return false;
@@ -58,7 +62,23 @@ public enum SimilarMode {
         if (item1Lore == null && item2Lore == null) return true;
         if (item1Lore == null || item2Lore == null) return false;
         
-        return item1Lore.equals(item2Lore);
+        if (item1Lore.size() != item2Lore.size()) return false;
+        if (item1Lore.isEmpty()) return true;
+        
+        for (int index = 0; index < item1Lore.size(); index++) {
+          Component item1LoreLine = item1Lore.get(index);
+          Component item2LoreLine = item2Lore.get(index);
+          
+          if (item1LoreLine == null && item2LoreLine == null) continue;
+          if (item1LoreLine == null || item2LoreLine == null) return false;
+          
+          String item1LoreLineText = LegacyComponentSerializer.legacySection().serialize(item1LoreLine);
+          String item2LoreLineText = LegacyComponentSerializer.legacySection().serialize(item2LoreLine);
+          
+          if (!item1LoreLineText.equals(item2LoreLineText)) return false;
+        }
+        
+        return true;
       }
       
       return false;
