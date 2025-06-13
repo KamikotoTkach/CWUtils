003bd0b0edaa68526b7292a4c6c32a9363cb060f
Danil Tkach <tkach@cwcode.ru>
2025-06-13 19:08:34 +0300

    add overloading with PersistentDataHolder instead of PersistentDataContainer in PersistentFieldWrapper

--- src/main/java/ru/cwcode/cwutils/persistent/PersistentFieldWrapper.java
+++ src/main/java/ru/cwcode/cwutils/persistent/PersistentFieldWrapper.java
@@ -2,20 +2,21 @@ package ru.cwcode.cwutils.persistent;
 
 import org.bukkit.NamespacedKey;
 import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataHolder;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Optional;
 
-public class PersistentFieldWrapper<T, Z> {
-  private final PersistentDataType<T, Z> type;
+public class PersistentFieldWrapper<Z> {
+  private final PersistentDataType<?, Z> type;
   private final NamespacedKey key;
   
-  public PersistentFieldWrapper(PersistentDataType<T, Z> type, NamespacedKey key) {
+  public PersistentFieldWrapper(PersistentDataType<?, Z> type, NamespacedKey key) {
     this.type = type;
     this.key = key;
   }
   
-  public PersistentFieldWrapper(PersistentDataType<T, Z> type, String key) {
+  public PersistentFieldWrapper(PersistentDataType<?, Z> type, String key) {
     this(type, NamespacedKey.fromString(key));
   }
   
@@ -44,5 +45,28 @@ public class PersistentFieldWrapper<T, Z> {
     Optional<Z> previous = get(container);
     container.remove(key);
     return previous;
+  }
+  
+  public Z getOrNull(PersistentDataHolder holder) {
+    return this.getOrNull(holder.getPersistentDataContainer());
+  }
+  
+  public Optional<Z> get(PersistentDataHolder holder) {
+    return this.get(holder.getPersistentDataContainer());
+  }
+  
+  /**
+   * @param value new value
+   * @return previous value if set
+   */
+  public Optional<Z> set(PersistentDataHolder holder, Z value) {
+    return this.set(holder.getPersistentDataContainer(), value);
+  }
+  
+  /**
+   * @return previous value if set
+   */
+  public Optional<Z> remove(PersistentDataHolder holder) {
+    return this.remove(holder.getPersistentDataContainer());
   }
 }
