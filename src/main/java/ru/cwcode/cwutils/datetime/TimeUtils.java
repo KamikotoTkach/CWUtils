42ad6529b593ee420bafcf0bd0cf4963672781dd
Danil Tkach <tkach@cwcode.ru>
2025-10-18 20:46:25 +0300

    add TimeUtils:isSameDay

--- src/main/java/ru/cwcode/cwutils/datetime/TimeUtils.java
+++ src/main/java/ru/cwcode/cwutils/datetime/TimeUtils.java
@@ -1,5 +1,7 @@
 package ru.cwcode.cwutils.datetime;
 
+import java.util.Calendar;
+
 public class TimeUtils {
   public static long unix() {
     return System.currentTimeMillis() / 1000;
@@ -11,5 +13,17 @@ public class TimeUtils {
   
   public static boolean isAfter(long millis) {
     return System.currentTimeMillis() > millis;
+  }
+  
+  public static boolean isSameDay(long millis1, long millis2) {
+    Calendar cal1 = Calendar.getInstance();
+    cal1.setTimeInMillis(millis1);
+    
+    Calendar cal2 = Calendar.getInstance();
+    cal2.setTimeInMillis(millis2);
+    
+    return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+           && cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH)
+           && cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
   }
 }
