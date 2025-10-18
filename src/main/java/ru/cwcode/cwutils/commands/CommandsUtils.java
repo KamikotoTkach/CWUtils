046d46bac3542a6070bf883eb4d4be6e14cd1913
Soul_KRT <soul.krt.00@gmail.com>
2025-10-18 19:35:53 +0300

    CommandUtils: - refactor, fix logic

--- src/main/java/ru/cwcode/cwutils/commands/CommandsUtils.java
+++ src/main/java/ru/cwcode/cwutils/commands/CommandsUtils.java
@@ -5,31 +5,37 @@ import java.util.Optional;
 
 public class CommandsUtils {
   public static boolean containsCommand(Collection<String> commands, String message) {
-    for (String command : commands) {
-      if (message.startsWith(command) || message.startsWith("/" + command)) {
-        return true;
-      }
-      
-      String[] cmd = message.split(":", 2);
-      if (cmd.length > 1) {
-        if (cmd[1].startsWith(command) || command.startsWith("/") && cmd[1].startsWith(command.substring(1))) {
-          return true;
-        }
-      }
-    }
-    
-    return false;
+    return getCommand(commands, message).isPresent();
   }
   
   public static Optional<String> getCommand(Collection<String> commands, String message) {
     for (String command : commands) {
-      if (message.startsWith(command) || message.startsWith("/" + command)) {
+      if (command.contains("*")) {
+        command = command.replace("*", "");
+        
+        if (message.startsWith(command) || message.startsWith("/" + command)) {
+          return Optional.of(command);
+        }
+        
+        String[] cmd = message.split(":", 2);
+        if (cmd.length > 1) {
+          if (cmd[1].startsWith(command) || command.startsWith("/") && cmd[1].startsWith(command.substring(1))) {
+            return Optional.of(command);
+          }
+        }
+        
+        continue;
+      }
+      
+      String cmd = message.split(" ")[0];
+      
+      if (cmd.equals(command) || cmd.equals("/" + command)) {
         return Optional.of(command);
       }
       
-      String[] cmd = message.split(":", 2);
-      if (cmd.length > 1) {
-        if (cmd[1].startsWith(command) || command.startsWith("/") && cmd[1].startsWith(command.substring(1))) {
+      String[] cmdAlias = cmd.split(":", 2);
+      if (cmdAlias.length > 1) {
+        if (cmdAlias[1].equals(command) || command.startsWith("/") && cmdAlias[1].equals(command.substring(1))) {
           return Optional.of(command);
         }
       }
